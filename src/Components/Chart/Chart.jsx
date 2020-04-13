1983dc212109091337486140c8e5afab56565355
rana-abid <abidmukhtar1212@gmail.com>
2020-04-13 15:35:29 +0500

    section-2 updated

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,6 +1,15 @@
-import React from "react"
+import React,{useState, useEffect} from "react";
+import {fetchDailyData } from "../../api/index";
 
 const Chart =  () => {
+    const [dailyData, setDailyData] = useState({});
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const dailyData = await fetchDailyData();
+
+        }
+    fetchAPI();
+    });
     return(
         <h1>Chart</h1>
     )
--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,19 @@ export const fetchData = async () => {
     try {
         const { data : {confirmed , recovered, deaths, lastUpdate }} = await axios.get(url);
 
-        const modifiedData = {confirmed , recovered, deaths, lastUpdate }
+        return {confirmed , recovered, deaths, lastUpdate }
 
-        return modifiedData;
     } catch (error) {
         
     }
 
 }
+
+export const fetchDailyDate = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+    } catch (error) {
+        
+    }
+}
+
